bec7b77ce5abe5a732a6278fe7ee101517878b1e
Satya Suravaram <satyasuravaram2020@gmail.com>
2020-07-06 21:42:59 -0500

    Fix mongoose deprecation

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
@@ -11,19 +11,19 @@ app.use(express.json());
 const mongo_URI = process.env.MONGO_URI;
 
 mongoose
-    .connect(mongo_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true })
-    .then(() => console.log("MongoDB connected..."))
-    .catch(err => console.log(err));
+  .connect(mongo_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 //Routes
-app.use("/users", require('./routes/users'));
-
+app.use("/users", require("./routes/users"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-
-})
+  console.log(`Server running on port ${port}`);
+});
