144118b156fb4aea69b383e0a330763a035b162d
Ajata Reddy <ajata.reddy@gmail.com>
2020-07-08 21:04:04 -0500

    Added connect button modal

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Modal from 'react-bootstrap/Modal';      
+import Form from 'react-bootstrap/Form';
  
 export default function Dashboard () {
     const [show, setShow] = React.useState(false);
@@ -39,51 +40,181 @@ export default function Dashboard () {
                     Bio stuff. Lorem ipsum blah blah blah, etc. im bored. hows life? fine. thank u. did i mention i was bored?
                     <Button variant="outline-danger" href="profile" style={{ marginLeft: '.5rem'}}>Read more</Button>
                     </Card.Text>
-                    <Button variant="primary" href="messages" style={{position: 'relative', left: '300px', bottom: '50px'}}>Connect</Button>
+                    <Button variant="primary" style={{position: 'relative', left: '300px', bottom: '50px'}} onClick={handleShow}>Connect</Button>
+                    
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>  
+                        <Modal.Title>Connect with tutor</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Label>Skill you need help with:</Form.Label>
+                                    <Form.Control placeholder="Skill" />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Issue:</Form.Label>
+                                    <Form.Control placeholder="Issue" />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" type="submit" onClick={handleClose}>
+                            Submit
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Card.Body>
             </Card>
             <Card style={{ width: '50rem' , marginLeft: 'auto', marginRight: 'auto', marginBottom: '20px'}}>
                 <Card.Img variant="left" src="holder.js/100px180?text=Image cap" />
                 <Card.Body style={{paddingBottom: '.1rem'}}>
                     <Card.Title style={{position: "absolute"}}>Name</Card.Title>
-                    <Card.Text style={{width: '80%'}}>
-                    Bio stuff.
+                    <Card.Text style={{width: '60%', marginLeft: 'auto', marginRight: 'auto', position: 'relative', right: '2rem'}}>
+                    Bio stuff. Lorem ipsum blah blah blah, etc. im bored. hows life? fine. thank u. did i mention i was bored?
                     <Button variant="outline-danger" href="profile" style={{ marginLeft: '.5rem'}}>Read more</Button>
                     </Card.Text>
-                    <Button variant="primary" href="messages" style={{position: 'relative', left: '300px', bottom: '50px'}}>Connect</Button>
+                    <Button variant="primary" style={{position: 'relative', left: '300px', bottom: '50px'}} onClick={handleShow}>Connect</Button>
+                    
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>  
+                        <Modal.Title>Connect with tutor</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Label>Skill you need help with:</Form.Label>
+                                    <Form.Control placeholder="Skill" />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Issue:</Form.Label>
+                                    <Form.Control placeholder="Issue" />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" type="submit" onClick={handleClose}>
+                            Submit
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Card.Body>
             </Card>
             <Card style={{ width: '50rem' , marginLeft: 'auto', marginRight: 'auto', marginBottom: '20px'}}>
                 <Card.Img variant="left" src="holder.js/100px180?text=Image cap" />
                 <Card.Body style={{paddingBottom: '.1rem'}}>
                     <Card.Title style={{position: "absolute"}}>Name</Card.Title>
-                    <Card.Text style={{width: '80%'}}>
-                    Bio stuff.
+                    <Card.Text style={{width: '60%', marginLeft: 'auto', marginRight: 'auto', position: 'relative', right: '2rem'}}>
+                    Bio stuff. Lorem ipsum blah blah blah, etc. im bored. hows life? fine. thank u. did i mention i was bored?
                     <Button variant="outline-danger" href="profile" style={{ marginLeft: '.5rem'}}>Read more</Button>
                     </Card.Text>
-                    <Button variant="primary" href="messages" style={{position: 'relative', left: '300px', bottom: '50px'}}>Connect</Button>
+                    <Button variant="primary" style={{position: 'relative', left: '300px', bottom: '50px'}} onClick={handleShow}>Connect</Button>
+                    
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>  
+                        <Modal.Title>Connect with tutor</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Label>Skill you need help with:</Form.Label>
+                                    <Form.Control placeholder="Skill" />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Issue:</Form.Label>
+                                    <Form.Control placeholder="Issue" />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" type="submit" onClick={handleClose}>
+                            Submit
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Card.Body>
             </Card>
             <Card style={{ width: '50rem' , marginLeft: 'auto', marginRight: 'auto', marginBottom: '20px'}}>
                 <Card.Img variant="left" src="holder.js/100px180?text=Image cap" />
                 <Card.Body style={{paddingBottom: '.1rem'}}>
                     <Card.Title style={{position: "absolute"}}>Name</Card.Title>
-                    <Card.Text style={{width: '80%'}}>
-                    Bio stuff.
+                    <Card.Text style={{width: '60%', marginLeft: 'auto', marginRight: 'auto', position: 'relative', right: '2rem'}}>
+                    Bio stuff. Lorem ipsum blah blah blah, etc. im bored. hows life? fine. thank u. did i mention i was bored?
                     <Button variant="outline-danger" href="profile" style={{ marginLeft: '.5rem'}}>Read more</Button>
                     </Card.Text>
-                    <Button variant="primary" href="messages" style={{position: 'relative', left: '300px', bottom: '50px'}}>Connect</Button>
+                    <Button variant="primary" style={{position: 'relative', left: '300px', bottom: '50px'}} onClick={handleShow}>Connect</Button>
+                    
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>  
+                        <Modal.Title>Connect with tutor</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Label>Skill you need help with:</Form.Label>
+                                    <Form.Control placeholder="Skill" />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Issue:</Form.Label>
+                                    <Form.Control placeholder="Issue" />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" type="submit" onClick={handleClose}>
+                            Submit
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Card.Body>
             </Card>
             <Card style={{ width: '50rem' , marginLeft: 'auto', marginRight: 'auto', marginBottom: '20px'}}>
                 <Card.Img variant="left" src="holder.js/100px180?text=Image cap" />
                 <Card.Body style={{paddingBottom: '.1rem'}}>
                     <Card.Title style={{position: "absolute"}}>Name</Card.Title>
-                    <Card.Text style={{width: '80%'}}>
-                    Bio stuff.
+                    <Card.Text style={{width: '60%', marginLeft: 'auto', marginRight: 'auto', position: 'relative', right: '2rem'}}>
+                    Bio stuff. Lorem ipsum blah blah blah, etc. im bored. hows life? fine. thank u. did i mention i was bored?
                     <Button variant="outline-danger" href="profile" style={{ marginLeft: '.5rem'}}>Read more</Button>
                     </Card.Text>
-                    <Button variant="primary" href="messages" style={{position: 'relative', left: '300px', bottom: '50px'}}>Connect</Button>
+                    <Button variant="primary" style={{position: 'relative', left: '300px', bottom: '50px'}} onClick={handleShow}>Connect</Button>
+                    
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>  
+                        <Modal.Title>Connect with tutor</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Label>Skill you need help with:</Form.Label>
+                                    <Form.Control placeholder="Skill" />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Issue:</Form.Label>
+                                    <Form.Control placeholder="Issue" />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" type="submit" onClick={handleClose}>
+                            Submit
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Card.Body>
             </Card>
         </div>
