b848c8e0f77ad514fa151c690b9adc6f4c54ead1
Satya Suravaram <satyasuravaram2020@gmail.com>
2020-07-16 00:10:23 -0500

    Update Action to account for existing connection

--- client/src/actions/CreateConnection.js
+++ client/src/actions/CreateConnection.js
@@ -1,14 +1,31 @@
 import Axios from "axios";
 
-async function createConnection(tutorID, userID) {
+async function createConnection(tutorID, userID, history) {
   try {
-    const connectionRes = await Axios.post(
-      "http://localhost:5000/messages/newConnection",
+    //Check if conversation already exists
+    const conversationRes = await Axios.post(
+      "http://localhost:5000/messages/findExisting",
       {
-        tutorId: tutorID,
-        currentUserId: userID,
+        tutorID: tutorID,
+        userID: userID,
       }
     );
+
+    if (conversationRes.data === false) {
+      //Create new Connection and Conversation
+      const connectionRes = await Axios.post(
+        "http://localhost:5000/messages/newConnection",
+        {
+          tutorId: tutorID,
+          currentUserId: userID,
+        }
+      );
+      console.log("New conversation created");
+      history.push(`/app/messages/${connectionRes.data}`)
+    } else {
+      console.log("Conversation already exists");
+      history.push(`/app/messages/${conversationRes.data}`)
+    }
   } catch (err) {
     console.log(err);
   }
