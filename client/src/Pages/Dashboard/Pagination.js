83058a17950ac7836dbbabe1b299149885d8483d
Ajata Reddy <ajata.reddy@gmail.com>
2020-07-26 18:04:33 -0500

    Add pagination sytem to the dashboard page

---
+++ client/src/Pages/Dashboard/Pagination.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Dashboard.css';
+
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav className = 'search-container'>
+      <ul className='pagination center'>
+        {pageNumbers.map(number => (
+          <li key={number} className='page-item'>
+            <a onClick={() => paginate(number)} href='#' className='page-link page-anchor'>
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
