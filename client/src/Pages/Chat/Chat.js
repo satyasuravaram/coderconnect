3e499a875b551bae3ce0c703845670c7dffc33b5
Satya Suravaram <satyasuravaram2020@gmail.com>
2020-08-01 13:05:49 -0500

    Add Styling to Chat Layout

--- client/src/Pages/Chat/Chat.js
+++ client/src/Pages/Chat/Chat.js
@@ -10,134 +10,134 @@ import "./Chat.css";
 import { socket } from "../../context/Socket";
 import CreateSession from "./CreateSession/CreateSession";
 import Whiteboard from "./Whiteboard/Whiteboard";
+import CodeEditor from "./CodeEditor/CodeEditor";
 
 export default function Chat() {
-	const [userID, setUserID] = useState("");
-	const [sessionInProgress, setSessionInProgress] = useState(false);
-	const [sessionType, setSessionType] = useState("Video");
-	const [messages, setMessages] = useState([]);
-	const ENDPOINT = "http://localhost:5000";
-	const { connectid } = useParams();
-	const videoRef = useRef();
-	const whiteboardRef = useRef();
-	const [leaveStatus, setLeaveStatus] = useState(false);
+  const [userID, setUserID] = useState("");
+  const [sessionInProgress, setSessionInProgress] = useState(false);
+  const [sessionType, setSessionType] = useState("Video");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "http://localhost:5000";
+  const { connectid } = useParams();
+  const videoRef = useRef();
+  const whiteboardRef = useRef();
+  const [leaveStatus, setLeaveStatus] = useState(false);
 
-	useEffect(() => {
-		const getUser = async () => {
-			const token = localStorage.getItem("auth-token");
-			const userRes = await Axios.get("http://localhost:5000/users/", {
-				headers: { "x-auth-token": token },
-			});
-			setUserID(userRes.data._id);
-		};
-		getUser();
-	}, []);
+  useEffect(() => {
+    const getUser = async () => {
+      const token = localStorage.getItem("auth-token");
+      const userRes = await Axios.get("http://localhost:5000/users/", {
+        headers: { "x-auth-token": token },
+      });
+      setUserID(userRes.data._id);
+    };
+    getUser();
+  }, []);
 
-	useEffect(() => {
-		socket.emit("join", { connectid }, () => {});
-	}, []);
+  useEffect(() => {
+    socket.emit("join", { connectid }, () => {});
+  }, []);
 
-	useEffect(() => {
-		const loadConversation = async () => {
-			try {
-				const loadRes = await Axios.get(
-					`http://localhost:5000/messages/${connectid}`
-				);
-				const existingMessages = loadRes.data;
-				for (let i = 0; i < existingMessages.length; i++) {
-					setMessages((oldMessages) => [...oldMessages, existingMessages[i]]);
-				}
-			} catch (err) {
-				console.log(err);
-			}
-		};
-		loadConversation();
-	}, []);
+  useEffect(() => {
+    const loadConversation = async () => {
+      try {
+        const loadRes = await Axios.get(
+          `http://localhost:5000/messages/${connectid}`
+        );
+        const existingMessages = loadRes.data;
+        for (let i = 0; i < existingMessages.length; i++) {
+          setMessages((oldMessages) => [...oldMessages, existingMessages[i]]);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    loadConversation();
+  }, []);
 
-	useEffect(() => {
-		socket.on("newMessage", (message) => {
-			const msgBody = message.message;
-			setMessages([...messages, msgBody]);
-		});
-	}, [messages]);
+  useEffect(() => {
+    socket.on("newMessage", (message) => {
+      const msgBody = message.message;
+      setMessages([...messages, msgBody]);
+    });
+  }, [messages]);
 
-	const handleLeave = (e) => {
-		e.preventDefault();
-		console.log(videoRef.current);
-		setSessionInProgress(false);
-	};
+  const handleLeave = (e) => {
+    e.preventDefault();
+    console.log(videoRef.current);
+    setSessionInProgress(false);
+  };
 
-	let sessionMedia;
-	if (sessionType === "Video") {
-		sessionMedia = (
-			<VideoChat
-				ref={videoRef}
-				room={connectid}
-				userID={userID}
-				leaveStatus={leaveStatus}
-			/>
-		);
-	} else if (sessionType === "Whiteboard") {
-		sessionMedia = <Whiteboard ref={whiteboardRef} />;
-	}
+  let sessionMedia;
+  if (sessionType === "Video") {
+    sessionMedia = (
+      <VideoChat
+        ref={videoRef}
+        room={connectid}
+        userID={userID}
+        leaveStatus={leaveStatus}
+      />
+    );
+  } else if (sessionType === "Whiteboard") {
+    sessionMedia = <Whiteboard ref={whiteboardRef} />;
+  }
 
-	const handleScreenSwitch = (type) => {
-		console.log(videoRef.current);
-		console.log(whiteboardRef.current);
-	};
+  const handleScreenSwitch = (type) => {
+    console.log(videoRef.current);
+    console.log(whiteboardRef.current);
+  };
 
-	return (
-		<div className="outer-chat-container">
-			{sessionInProgress && (
-				<div className="call-options">
-					{/* <Button
-						color="outline-primary"
-						href="#video"
-					>
-						Video Call
-					</Button> */}
-					<Button id="btn-whiteboard" color="primary" href="#header-whiteboard">
-						Whiteboard
-					</Button>
-					<Button id="btn-leave" color="danger" onClick={(e) => handleLeave(e)}>
-						Leave Room
-					</Button>
-				</div>
-			)}
-			<div className="chat-container">
-				<div className="messages-container">
-					<Messages messages={messages} userID={userID} />
-					<Input room={connectid} userID={userID} />
-				</div>
-				<div className="session-container">
-					{!sessionInProgress ? (
-						<CreateSession setSessionInProgress={setSessionInProgress} />
-					) : (
-						<VideoChat
-							ref={videoRef}
-							room={connectid}
-							userID={userID}
-							leaveStatus={leaveStatus}
-						/>
-					)}
-				</div>
-			</div>
-			{sessionInProgress && (
-				<>
-					<h2 id="header-whiteboard">Whiteboard</h2>
-					<Whiteboard />
-				</>
-			)}
-		</div>
-	);
+  return (
+    <div className="outer-chat-container">
+      {sessionInProgress && (
+        <div className="call-options">
+          <Button id="btn-code" color="primary" href="#code-playground">
+            Code Playground
+          </Button>
+          <Button id="btn-whiteboard" href="#whiteboard">
+            Whiteboard
+          </Button>
+          <Button id="btn-leave" color="danger" onClick={(e) => handleLeave(e)}>
+            Leave Room
+          </Button>
+        </div>
+      )}
+      <div className="chat-container">
+        <div className="messages-container">
+          <Messages messages={messages} userID={userID} />
+          <Input room={connectid} userID={userID} />
+        </div>
+        <div className="session-container">
+          {!sessionInProgress ? (
+            <CreateSession setSessionInProgress={setSessionInProgress} />
+          ) : (
+            <VideoChat
+              ref={videoRef}
+              room={connectid}
+              userID={userID}
+              leaveStatus={leaveStatus}
+            />
+          )}
+        </div>
+      </div>
+      {sessionInProgress && (
+        <>
+          <h2 id="code-playground">Code Playground</h2>
+          <CodeEditor room={connectid} />
+          <h2 id="whiteboard">Whiteboard</h2>
+          <Whiteboard />
+        </>
+      )}
+    </div>
+  );
 }
 
 {
-	/* <VideoChat room={connectid} userID={userID} /> */
+  /* <VideoChat room={connectid} userID={userID} /> */
 }
 
 {
-	/* <div className="outerContainer">
+  /* <div className="outerContainer">
       <div className="innerContainer">
         <Messages messages={messages} userID={userID} />
         <Input room={connectid} userID={userID} />
