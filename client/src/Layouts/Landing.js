3ff8ddf35d16a516ce797f6189559be998e84679
Satya Suravaram <satyasuravaram2020@gmail.com>
2020-08-04 14:34:37 -0500

    Minor styling fixes

--- client/src/Layouts/Landing.js
+++ client/src/Layouts/Landing.js
@@ -8,14 +8,13 @@ import Faq from "../Pages/FAQ/FAQ";
 import Home from "../Pages/Home/Home";
 import Register from "../Pages/Register/Register";
 import Login from "../Pages/Login/Login";
-
 import LandingNavbar from "../components/LandingNavbar";
-
+import "./Landing.css";
 function Landing() {
   return (
     <React.Fragment>
       <LandingNavbar />
-      <div className = "content-container">
+      <div>
         <Switch>
           <Route path="/become-a-tutor" component={BecomeTutor}></Route>
           <Route path="/about" component={About}></Route>
