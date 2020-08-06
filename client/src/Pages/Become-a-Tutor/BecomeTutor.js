147ce65758cd47af8942cf0ed3ea4c686d5ace48
Andrew Shang <ashang1524@gmail.com>
2020-08-05 19:52:55 -0500

    Add UI Design and Layout for Become Tutor component

--- client/src/Pages/Become-a-Tutor/BecomeTutor.js
+++ client/src/Pages/Become-a-Tutor/BecomeTutor.js
@@ -51,119 +51,110 @@ export default function BecomeTutor() {
       setValidated(true);
     }
   };
-  return (<div>
-    <h2>Become a Tutor</h2>
-    {error && <ErrorNotice message={error} />}
-    {success && <SuccessNotice message={success} />}
-    <Form id="tutor-form" noValidate validated={validated} onSubmit={handleSubmit}>
-      <Form.Row>
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="First name"
-            onChange={e => setFirstName(e.target.value)}
-          />
-          <Form.Control.Feedback type="valid">
-            Looks good!
-              </Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            Please input your first name.
-              </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Last name"
-            onChange={e => setLastName(e.target.value)}
-          />
-          <Form.Control.Feedback type="valid">
-            Looks good!
-              </Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            Please input your last name.
-              </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustomEmail">
-          <Form.Label>Email</Form.Label>
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-            </InputGroup.Prepend>
-            <Form.Control
-              required
-              type="text"
-              placeholder="Last name"
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            <Form.Control.Feedback type="valid">
-              Looks good!
-            </Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid">
-              Please input your last name.
-            </Form.Control.Feedback>
-            </InputGroup>
-          </Form.Group>
-          <Form.Group as={Col} md="4" controlId="validationCustomEmail">
-            <Form.Label>Email</Form.Label>
-            <InputGroup>
-              <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-              </InputGroup.Prepend>
-              <Form.Control
-                type="email"
-                placeholder="foo@gmail.com"
-                aria-describedby="inputGroupPrepend"
+  return (
+    <div className="bt-outer-container">
+      <div className="bt-inner-container">
+        <div className="bt-form">
+          <h2>Become a Tutor</h2>
+          {error && <ErrorNotice message={error} />}
+          {success && <SuccessNotice message={success} />}
+          <Form
+            id="tutor-form"
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+          >
+            <Form.Row>
+              <Form.Group as={Col} controlId="validationCustom01">
+                <Form.Label>First name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="First name"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <Form.Control.Feedback type="valid">
+                  Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  Please input your first name.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} controlId="validationCustom02">
+                <Form.Label>Last name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Last name"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                <Form.Control.Feedback type="valid">
+                  Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  Please input your last name.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} controlId="validationCustomEmail">
+                <Form.Label>Email</Form.Label>
+                <InputGroup>
+                  <Form.Control
+                    type="email"
+                    placeholder="foo@gmail.com"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <Form.Control.Feedback type="valid">
+                    Looks good!
+                  </Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    Please input your email address.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+              <Form.Group as={Col} md="3" controlId="validationCustom05">
+                <Form.Label>Age</Form.Label>
+                <Form.Control
+                  onChange={(e) => setAge(e.target.value)}
+                  type="number"
+                  placeholder="Age"
+                  required
+                />
+                <Form.Control.Feedback type="valid">
+                  Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid age.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+            <Form.Group as={Col} controlId="validationCustom03">
+              <Form.File
+                id="resume"
+                label="Resume/CV"
+                onChange={(e) => setResume(e.target.files[0])}
                 required
-                onChange={(e) => setEmail(e.target.value)}
               />
-              <Form.Control.Feedback type="valid">
-                Looks good!
-              </Form.Control.Feedback>
-              <Form.Control.Feedback type="invalid">
-                Please input your email address.
-              </Form.Control.Feedback>
-            </InputGroup>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} md="3" controlId="validationCustom03">
-            <Form.File
-              id="resume"
-              label="Please put your resume or CV here"
-              onChange={(e) => setResume(e.target.files[0])}
-              required
-            />
-          </Form.Group>
-          <Form.Group as={Col} md="3" controlId="validationCustom05">
-            <Form.Label>Age</Form.Label>
-            <Form.Control
-              onChange={(e) => setAge(e.target.value)}
-              type="number"
-              placeholder="Age"
-              required
-            />
-            <Form.Control.Feedback type="valid">
-              Looks good!
-            </Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid age.
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} controlId="validationCustom06">
-            <Form.Label>Skills</Form.Label>
-            <Skills setSkills={setSkills} required />
-          </Form.Group>
-        </Form.Row>
+            </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} controlId="validationCustom06">
+                <Form.Label>Skills</Form.Label>
+                <Skills setSkills={setSkills} required />
+              </Form.Group>
+            </Form.Row>
 
-        <hr />
-        <Button type="submit">Submit app</Button>
-      </Form>
+            <hr />
+            <Button type="submit">Submit app</Button>
+          </Form>
+        </div>
+        <div className="bt-right-half">hi</div>
+      </div>
     </div>
   );
 }
