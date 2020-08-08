2f7271b2c916b6153bd45842fed041b44612184d
Ajata Reddy <ajata.reddy@gmail.com>
2020-08-07 19:41:43 -0500

    Style About, Become Tutor, Contact, and FAQ

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -1,29 +1,85 @@
-import React from 'react';
-import {Card, CardColumns, Accordion} from 'react-bootstrap';
-import './About.css';
+import React from "react";
+import { Card, CardColumns, Accordion } from "react-bootstrap";
+import "./About.css";
 import satya from "./images/satya.jpeg";
-import ajata from "./images/ajata.jpeg"
+import ajata from "./images/ajata.jpeg";
 import karthik from "./images/karthik.jpeg";
 
-
-export default function About () {
-    return <div class="container">
-        <h2 class="title">About Us</h2>
-        <div class="person">
-            <img src={satya} alt="satya-img" class="person-image"/>
-            <h3 class="name">Satya Suravaram</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce scelerisque vehicula risus eget molestie. Curabitur ut bibendum diam. Vivamus et nulla vulputate neque finibus dictum sed non odio. Etiam magna justo, tempor eget nisl ullamcorper, euismod vulputate est. Aliquam varius justo congue orci bibendum elementum. Proin lacinia convallis nisi in congue. Curabitur faucibus ultrices eros vel faucibus. Ut suscipit nibh mattis sem condimentum, in feugiat orci luctus. Sed vel condimentum massa. Praesent id leo facilisis, tempor diam vitae, tristique eros. Fusce vitae dui in augue dictum dapibus nec a enim. Integer sed tellus non metus ullamcorper tempus. Integer rutrum ut massa in hendrerit. Aenean volutpat ac arcu tincidunt porta.</p>
-        </div>
-        <div class="person">
-            <img src={ajata} alt="ajata-img" class="person-image"/>
-            <h3 class="name">Ajata Reddy</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce scelerisque vehicula risus eget molestie. Curabitur ut bibendum diam. Vivamus et nulla vulputate neque finibus dictum sed non odio. Etiam magna justo, tempor eget nisl ullamcorper, euismod vulputate est. Aliquam varius justo congue orci bibendum elementum. Proin lacinia convallis nisi in congue. Curabitur faucibus ultrices eros vel faucibus. Ut suscipit nibh mattis sem condimentum, in feugiat orci luctus. Sed vel condimentum massa. Praesent id leo facilisis, tempor diam vitae, tristique eros. Fusce vitae dui in augue dictum dapibus nec a enim. Integer sed tellus non metus ullamcorper tempus. Integer rutrum ut massa in hendrerit. Aenean volutpat ac arcu tincidunt porta.</p>
-        </div>
-        <div class="person">
-            <img src={karthik} alt="karthik-img" class="person-image"/>
-            <h3 class="name">Karthik Menon</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce scelerisque vehicula risus eget molestie. Curabitur ut bibendum diam. Vivamus et nulla vulputate neque finibus dictum sed non odio. Etiam magna justo, tempor eget nisl ullamcorper, euismod vulputate est. Aliquam varius justo congue orci bibendum elementum. Proin lacinia convallis nisi in congue. Curabitur faucibus ultrices eros vel faucibus. Ut suscipit nibh mattis sem condimentum, in feugiat orci luctus. Sed vel condimentum massa. Praesent id leo facilisis, tempor diam vitae, tristique eros. Fusce vitae dui in augue dictum dapibus nec a enim. Integer sed tellus non metus ullamcorper tempus. Integer rutrum ut massa in hendrerit. Aenean volutpat ac arcu tincidunt porta.</p>
-        </div>
-
-        </div>;
-};
+export default function About() {
+  return(
+  <div className="about-outer-container">
+    <div class="about-container-top">
+      <h2 class="title">About Us</h2>
+    </div>
+    <div className="about-container-bottom">
+      <div class="person">
+        <img src={satya} alt="satya-img" class="person-image" />
+        <h3 class="name">Satya Suravaram</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          scelerisque vehicula risus eget molestie. Curabitur ut bibendum diam.
+          Vivamus et nulla vulputate neque finibus dictum sed non odio. Etiam
+          magna justo, tempor eget nisl ullamcorper, euismod vulputate est.
+          Aliquam varius justo congue orci bibendum elementum. Proin lacinia
+          convallis nisi in congue. Curabitur faucibus ultrices eros vel
+          faucibus. Ut suscipit nibh mattis sem condimentum, in feugiat orci
+          luctus. Sed vel condimentum massa. Praesent id leo facilisis, tempor
+          diam vitae, tristique eros. Fusce vitae dui in augue dictum dapibus
+          nec a enim. Integer sed tellus non metus ullamcorper tempus. Integer
+          rutrum ut massa in hendrerit. Aenean volutpat ac arcu tincidunt porta.
+        </p>
+      </div>
+      <div class="person">
+        <img src={ajata} alt="ajata-img" class="person-image" />
+        <h3 class="name">Ajata Reddy</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          scelerisque vehicula risus eget molestie. Curabitur ut bibendum diam.
+          Vivamus et nulla vulputate neque finibus dictum sed non odio. Etiam
+          magna justo, tempor eget nisl ullamcorper, euismod vulputate est.
+          Aliquam varius justo congue orci bibendum elementum. Proin lacinia
+          convallis nisi in congue. Curabitur faucibus ultrices eros vel
+          faucibus. Ut suscipit nibh mattis sem condimentum, in feugiat orci
+          luctus. Sed vel condimentum massa. Praesent id leo facilisis, tempor
+          diam vitae, tristique eros. Fusce vitae dui in augue dictum dapibus
+          nec a enim. Integer sed tellus non metus ullamcorper tempus. Integer
+          rutrum ut massa in hendrerit. Aenean volutpat ac arcu tincidunt porta.
+        </p>
+      </div>
+      <div class="person">
+        <img src={karthik} alt="karthik-img" class="person-image" />
+        <h3 class="name">Karthik Menon</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          scelerisque vehicula risus eget molestie. Curabitur ut bibendum diam.
+          Vivamus et nulla vulputate neque finibus dictum sed non odio. Etiam
+          magna justo, tempor eget nisl ullamcorper, euismod vulputate est.
+          Aliquam varius justo congue orci bibendum elementum. Proin lacinia
+          convallis nisi in congue. Curabitur faucibus ultrices eros vel
+          faucibus. Ut suscipit nibh mattis sem condimentum, in feugiat orci
+          luctus. Sed vel condimentum massa. Praesent id leo facilisis, tempor
+          diam vitae, tristique eros. Fusce vitae dui in augue dictum dapibus
+          nec a enim. Integer sed tellus non metus ullamcorper tempus. Integer
+          rutrum ut massa in hendrerit. Aenean volutpat ac arcu tincidunt porta.
+        </p>
+      </div>
+      <div class="person">
+        <img src={karthik} alt="andrew-img" class="person-image" />
+        <h3 class="name">Andrew Shang</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          scelerisque vehicula risus eget molestie. Curabitur ut bibendum diam.
+          Vivamus et nulla vulputate neque finibus dictum sed non odio. Etiam
+          magna justo, tempor eget nisl ullamcorper, euismod vulputate est.
+          Aliquam varius justo congue orci bibendum elementum. Proin lacinia
+          convallis nisi in congue. Curabitur faucibus ultrices eros vel
+          faucibus. Ut suscipit nibh mattis sem condimentum, in feugiat orci
+          luctus. Sed vel condimentum massa. Praesent id leo facilisis, tempor
+          diam vitae, tristique eros. Fusce vitae dui in augue dictum dapibus
+          nec a enim. Integer sed tellus non metus ullamcorper tempus. Integer
+          rutrum ut massa in hendrerit. Aenean volutpat ac arcu tincidunt porta.
+        </p>
+      </div>
+    </div>
+  </div>
+  )}
--- client/src/Pages/Contact/Contact.js
+++ client/src/Pages/Contact/Contact.js
@@ -1,111 +1,131 @@
 import React, { useState, validated, handleSubmit } from "react";
 import { Form, Col, InputGroup, Button } from "react-bootstrap";
-import "./Contact.css"
+import "./Contact.css";
 import { makeToast, endSessionToast } from "../../components/misc/Toaster";
 import Axios from "axios";
 import SuccessNotice from "../../components/misc/SuccessNotice";
 
-
 export default function Contact() {
-    const [validated, setValidated] = useState(true);
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
-    const [email, setEmail] = useState();
-    const [message, setMessage] = useState();
-    const [success, setSuccess] = useState("");
+  const [validated, setValidated] = useState(true);
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+  const [success, setSuccess] = useState("");
 
-    const handleSubmit = (e) => {
-        const form = e.currentTarget;
-        e.preventDefault();
-        if (form.checkValidity() === true) {
-            console.log(firstName, lastName, email, message);
-            Axios.post('http://localhost:5000/mail/contact', {
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                message: message
-            })
-                .then(function (response) {
-                    console.log(response);
-                })
-            setValidated(false);
-            document.getElementById("contact-form").reset();
-            setSuccess("Form submitted successfully!");
-        } else {
-            e.stopPropagation();
-            setValidated(true);
-        }
-    };
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    e.preventDefault();
+    if (form.checkValidity() === true) {
+      console.log(firstName, lastName, email, message);
+      Axios.post("http://localhost:5000/mail/contact", {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        message: message,
+      }).then(function (response) {
+        console.log(response);
+      });
+      setValidated(false);
+      document.getElementById("contact-form").reset();
+      setSuccess("Form submitted successfully!");
+    } else {
+      e.stopPropagation();
+      setValidated(true);
+    }
+  };
 
-    return <div>
-        <h2>Contact Us</h2>
-        {success && <SuccessNotice message={success} />}
-        <Form id="contact-form" noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Row>
-                <Form.Group as={Col} md="4" controlId="validationCustom01">
-                    <Form.Label>First name</Form.Label>
-                    <Form.Control
-                        required
-                        type="text"
-                        placeholder="First name"
-                        onChange={e => setFirstName(e.target.value)}
-                    />
-                    <Form.Control.Feedback type="valid">
-                        Looks good!
-              </Form.Control.Feedback>
-                    <Form.Control.Feedback type="invalid">
-                        Please input your first name.
-              </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="4" controlId="validationCustom02">
-                    <Form.Label>Last name</Form.Label>
-                    <Form.Control
-                        required
-                        type="text"
-                        placeholder="Last name"
-                        onChange={e => setLastName(e.target.value)}
-                    />
-                    <Form.Control.Feedback type="valid">
-                        Looks good!
-              </Form.Control.Feedback>
-                    <Form.Control.Feedback type="invalid">
-                        Please input your last name.
-              </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="4" controlId="validationCustomEmail">
-                    <Form.Label>Email</Form.Label>
-                    <InputGroup>
-                        <InputGroup.Prepend>
-                            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                            type="email"
-                            placeholder="foo@gmail.com"
-                            aria-describedby="inputGroupPrepend"
-                            required
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        <Form.Control.Feedback type="valid">
-                            Looks good!
-                        </Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">
-                            Please input your email address.
-                        </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
-            </Form.Row>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Message</Form.Label>
-                <Form.Control required as="textarea" rows="3" onChange={e => setMessage(e.target.value)} />
-                <Form.Control.Feedback type="valid">
+  return (
+    <div>
+      <div className="bt-outer-container-contact">
+        <div className="bt-inner-container-contact">
+          <div className="bt-form-contact">
+            <h2>Contact Us</h2>
+            {success && <SuccessNotice message={success} />}
+            <Form
+              id="contact-form"
+              noValidate
+              validated={validated}
+              onSubmit={handleSubmit}
+            >
+              <Form.Row>
+                <Form.Group as={Col} md="6" controlId="validationCustom01">
+                  <Form.Label>First name</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="First name"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                  <Form.Control.Feedback type="valid">
                     Looks good!
+                  </Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    Please input your first name.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="6" controlId="validationCustom02">
+                  <Form.Label>Last name</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Last name"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                  <Form.Control.Feedback type="valid">
+                    Looks good!
+                  </Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    Please input your last name.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} controlId="validationCustomEmail">
+                  <Form.Label>Email</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="inputGroupPrepend">
+                        @
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      type="email"
+                      placeholder="foo@gmail.com"
+                      aria-describedby="inputGroupPrepend"
+                      required
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Form.Control.Feedback type="valid">
+                      Looks good!
+                    </Form.Control.Feedback>
+                    <Form.Control.Feedback type="invalid">
+                      Please input your email address.
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+              </Form.Row>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                  required
+                  as="textarea"
+                  rows="3"
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+                <Form.Control.Feedback type="valid">
+                  Looks good!
                 </Form.Control.Feedback>
                 <Form.Control.Feedback type="invalid">
-                    Please input your message
+                  Please input your message
                 </Form.Control.Feedback>
-            </Form.Group>
-            <hr />
-            <Button type="submit">Submit message</Button>
-        </Form>
+              </Form.Group>
+              <hr />
+              <Button type="submit">Submit message</Button>
+            </Form>
+          </div>
+        </div>
+      </div>
     </div>
+  );
 }
