3a755f65279041569d95070a7146f8e03e210332
Ajata Reddy <ajata.reddy@gmail.com>
2020-09-05 13:09:11 -0500

    Fix https bugs

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const enforce = require('express-sslify');
 app.use(cors());
 app.use(express.json());
 
-app.use(enforce.HTTPS()); //forces https
+app.use(enforce.HTTPS({ trustProtoHeader: true })); //forces https
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -108,6 +108,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => {
+server.listen(app.get(port), () => {
   console.log(`Server running on port ${port}`);
 });
